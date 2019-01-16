f4bf395dfac2660615053e5c5cfcdc528d4d66aa
Gon <cliffsgkim@gmail.com>
2019-01-16 14:44:07 +0900

    main 영화 model에 담음

--- Final_Project/src/main/java/com/ob/view/controller/PageChangeController.java
+++ Final_Project/src/main/java/com/ob/view/controller/PageChangeController.java
@@ -1,18 +1,24 @@
 package com.ob.view.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.ob.biz.service.MovieService;
 
 @Controller
 //"Logininformation"라는 이름의 Model이 있으면 session에 저장
 @SessionAttributes("Logininformation")
 public class PageChangeController {
-	
+	@Autowired 
+	private MovieService movieService;
 	
 	@RequestMapping(value = "/mainGO.do", method = {RequestMethod.GET, RequestMethod.POST})
-	public String mainGO() {
+	public String mainGO(Model model) {
+		model.addAttribute("movieListOnair",movieService.getMovieListOnair());
 		return "/views/main/main.jsp";
 	}
 	@RequestMapping(value = "/joinUS.do", method = {RequestMethod.GET, RequestMethod.POST})
